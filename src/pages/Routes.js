6e162717d7f2cd219906bc026c9b19e299db3bb5
Delta <ti22mau@gmail.com>
2023-07-17 19:24:59 -0300

    fixed url problem

--- src/pages/Routes.js
+++ src/pages/Routes.js
@@ -1,16 +1,17 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./Home";
 import PokemonDetails from "./PokemonDetails";
 
 function AppRoutes() {
-    return(
-        <BrowserRouter>
-    <Routes>
-      <Route path="/Home" element={<Home />} />
-      <Route path="/Pokemon/:name" element={<PokemonDetails />} />
-    </Routes>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/home" />} />
+        <Route path="/Home" element={<Home />} />
+        <Route path="/Pokemon/:name" element={<PokemonDetails />} />
+      </Routes>
     </BrowserRouter>
-    )
+  )
 }
 
 export default AppRoutes
